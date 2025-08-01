0c30eea89436691810398347b9e77f8d41c1fadb
Tarun Shetty <tshetty2020@gmail.com>
2025-08-01 21:05:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-};
+import { firebaseConfig } from "./firebaseConfig";
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,11 @@
+// This file is intended to be used for public-facing Firebase credentials.
+// Security is handled by Firebase Security Rules and App Check.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6KBCNa3Q3qXxeMBsrNp4FR71QFZmXHAM",
+  authDomain: "getyourtrials.firebaseapp.com",
+  projectId: "getyourtrials",
+  storageBucket: "getyourtrials.firebasestorage.app",
+  messagingSenderId: "851322843134",
+  appId: "1:851322843134:web:a4b43366b4db27c2b59aaf",
+};
